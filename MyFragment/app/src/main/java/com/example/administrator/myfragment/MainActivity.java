106f98a2e258c4fa6b516b15b1f73d3207de3b70
haibowen <1461154748@qq.com>
2018-11-01 14:53:23 +0800

    test the fragment haibowen

--- MyFragment/app/src/main/java/com/example/administrator/myfragment/MainActivity.java
+++ MyFragment/app/src/main/java/com/example/administrator/myfragment/MainActivity.java
@@ -1,13 +1,50 @@
 package com.example.administrator.myfragment;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+  private     Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button=findViewById(R.id.bt_left_fragment);
+        button.setOnClickListener(this);
+        replaceFragment(new RightFragment());
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.bt_left_fragment:
+                replaceFragment(new AnotherFragment());
+                break;
+                default:
+                    break;
+
+
+        }
+
+    }
+
+    public  void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction transaction=fragmentManager.beginTransaction();
+        transaction.replace(R.id.right_fragment,fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+
     }
 }
