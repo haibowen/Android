21d562f03f72c546f46b41c724e658561851ca4f
haibowen <1461154748@qq.com>
2018-11-09 14:44:29 +0800

    webview

---
+++ MyInternet/app/src/main/java/com/example/administrator/myinternet/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.administrator.myinternet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView=findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.baidu.com");
+
+    }
+}
