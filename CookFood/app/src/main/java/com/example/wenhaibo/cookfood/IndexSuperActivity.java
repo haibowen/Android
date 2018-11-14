6ab991d356fe6dcae82f0ce31bc4ba5f0dffe6da
haibowen <1461154748@qq.com>
2018-11-14 10:52:25 +0800

    做点美食

---
+++ CookFood/app/src/main/java/com/example/wenhaibo/cookfood/IndexSuperActivity.java
@@ -0,0 +1,13 @@
+package com.example.wenhaibo.cookfood;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IndexSuperActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index);
+    }
+}
