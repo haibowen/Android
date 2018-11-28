13aed0255985c91bb8b2aa3ca560228e180884e3
haibowen <1461154748@qq.com>
2018-11-28 16:22:32 +0800

    View 的学习记录

--- Myview/app/src/main/java/com/example/administrator/myview/CircleView.java
+++ Myview/app/src/main/java/com/example/administrator/myview/CircleView.java
@@ -1,6 +1,7 @@
-package com.example.administrator.myviewmove;
+package com.example.administrator.myview;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -9,7 +10,7 @@ import android.util.AttributeSet;
 import android.view.View;
 
 public class CircleView extends View {
-    private int mcolor= Color.RED;
+    private int mcolor=Color.GREEN;
     private Paint mpaint=new Paint(Paint.ANTI_ALIAS_FLAG);
 
 
@@ -25,6 +26,9 @@ public class CircleView extends View {
 
     public CircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        TypedArray a=context.obtainStyledAttributes(attrs,R.styleable.CircleView);
+        mcolor=a.getColor(R.styleable.CircleView_cicle_color,Color.RED);
+        a.recycle();
         init();
     }
     private void  init(){
@@ -35,9 +39,13 @@ public class CircleView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        int width=getWidth();
-        int height=getHeight();
+        final  int paddingleft=getPaddingLeft();
+        final  int paddingright=getPaddingRight();
+        final  int paddingtop=getPaddingTop();
+        final int paddinbottom=getPaddingBottom();
+        int width=getWidth()-paddingleft-paddingright;
+        int height=getHeight()-paddingtop-paddinbottom;
         int radius=Math.min(width,height)/2;
-        canvas.drawCircle(width/2,height/2,radius,mpaint);
+        canvas.drawCircle(paddingleft+width/2,paddingtop+height/2,radius,mpaint);
     }
 }
--- Myview/app/src/main/java/com/example/administrator/myview/MainActivity.java
+++ Myview/app/src/main/java/com/example/administrator/myview/MainActivity.java
@@ -1,13 +1,88 @@
 package com.example.administrator.myview;
 
-import android.support.v7.app.AppCompatActivity;
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.bt_move)
+    public Button button;
+
+
+
+
+    @OnClick(R.id.bt_move)
+    public void change() {
+        Toast.makeText(MainActivity.this, "wobeidinjile", Toast.LENGTH_SHORT).show();
+        Log.d("wenhaibo", "change: " + "2222222222222");
+
+        ObjectAnimator.ofFloat(button, "translationX",
+                0, 100).setDuration(100).start();
+        Intent intent=new Intent(this,Main2Activity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.enter,R.anim.exit);
+
+
+
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+        button.setBackgroundResource(R.drawable.animation);
+        AnimationDrawable drawable= (AnimationDrawable) button.getBackground();
+        drawable.start();
+
+
+        /**
+         public boolean onTouchEvent(MotionEvent event){
+
+         int x= (int) event.getRawX();
+         int y= (int) event.getRawY();
+         switch ( event.getAction()){
+         case MotionEvent.ACTION_DOWN:
+
+         //
+         break;
+         case MotionEvent.ACTION_MOVE:
+         int deltax=x-mlastx;
+         int deltay=y-mlasty;
+
+         int translationX= ViewDragHelper.getTranslationX(this)+deltax;
+         int translationY= ViewDragHelper.getTranslationY(this)+deltay;
+         ViewDragHelper.setTranslationX(this,translationX);
+         ViewDragHelper.setTranslationY(this,translationY);
+         break;
+         case MotionEvent.ACTION_UP:
+         break;
+         default:
+         break;
+
+
+
+
+         }
+         mlastx=x;
+         mlasty=y;
+         return true;
+
+
+         }
+         **/
     }
 }
