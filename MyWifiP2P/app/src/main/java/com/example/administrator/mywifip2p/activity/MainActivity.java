479d367e1ea998628fc2a3ef641cb64afe34fd29
haibowen <1461154748@qq.com>
2018-12-14 18:15:55 +0800

    wifiP2p

---
+++ MyWifiP2P/app/src/main/java/com/example/administrator/mywifip2p/activity/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.administrator.mywifip2p.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.administrator.mywifip2p.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
